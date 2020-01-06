9fef33593bdc0b3e62a97a0f4d86d3390b9f4e1f
jingxuelong <jingxuelong0037@gmail.com>
2020-01-06 17:53:28 +0800

    framework commit

---
+++ XJSwiftFramework.framework/Headers/XJSwiftFramework.h
@@ -0,0 +1,18 @@
+//
+//  XJSwiftFramework.h
+//  XJSwiftFramework
+//
+//  Copyright © 2018 JingXueLong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XJSwiftFramework.
+FOUNDATION_EXPORT double XJSwiftFrameworkVersionNumber;
+
+//! Project version string for XJSwiftFramework.
+FOUNDATION_EXPORT const unsigned char XJSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XJSwiftFramework/PublicHeader.h>
+
+
